ea18e9f55f5759ca114be712f87bd5ed689e2c76
Veldi <adityaveldi@gmail.com>
2020-05-21 00:19:49 +0530

    program updated

---
+++ venv/Scripts/inputtype.py
@@ -0,0 +1,12 @@
+# here we see how to know the class of the variable
+# initialize the variable
+#suppose the variable be dtype
+dtype=21
+#we use "type()" to know the class of the variable as shown below
+print(type(dtype))
+# in python everything is an object which may be mutable or immutable
+dtype='welcome to github'
+#here dtype reference variable of an immutable object therefore the content in the object remains same but the refrence variable
+#dtype now pointing to the string object containing some data.
+# so built in types like int,float,bool,str,tuple etc are the immutable objects
+print(type(dtype))
