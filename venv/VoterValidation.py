15f08c4320c10739bdf2731690b9f7c7101171ec
Veldi <adityaveldi@gmail.com>
2020-05-21 00:41:45 +0530

    program updated

---
+++ venv/VoterValidation.py
@@ -0,0 +1,15 @@
+# we will see how to take input from user.
+name=input("Enter your name ")
+# to ristrict the user to specific data we do as shown below.
+age=int(input("Enter your age"))
+#user can enter integer type as input for age variable.
+if(age>=18):
+    print("Hey", name,"your eligible for voting")
+
+else:
+    if(age>0 and age<18):
+        calc=18-age
+        print("sorry you have to wait more",calc,"year/years")
+
+
+
