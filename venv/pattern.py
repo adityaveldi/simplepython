2fc4bb9ebdc0b6303761422314350963b3093dcc
Veldi <adityaveldi@gmail.com>
2020-05-21 10:30:15 +0530

    program updated

--- venv/pattern.py
+++ venv/pattern.py
@@ -1,7 +1,7 @@
 # now we shall see hoe to make use of loops to print a pattern in python
+#in python indentation is very important
 value=int(input("Enter the value"))
 for i in range(value+1):
     for j in range(i):
         print("*",end=' ')
-
     print(end='\n')
