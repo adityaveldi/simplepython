08fdd760dd87242aad4ff8cd0a523e6c72553851
Veldi <adityaveldi@gmail.com>
2020-05-29 13:48:38 +0530

    program updated

---
+++ venv/functions.py
@@ -0,0 +1,16 @@
+# Now let us see how to use functions in python
+#dont
+import numpy as np
+import matplotlib.pyplot as plt
+c=[1,2,3,4]
+print(c)
+array = np.array(c)
+arr= array
+print(arr)
+print(arr.size)
+a=np.arange(1,10,2,float)
+print(a)
+x=[1,2,3,4,5]
+y=[0.0,0.2,0.4,0.5,0.9]
+plt.plot(x,y,color="red")
+plt.show()
