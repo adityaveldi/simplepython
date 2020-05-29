d7087317f4b088736b30eacd88c282be764d7302
Veldi <adityaveldi@gmail.com>
2020-05-29 17:37:42 +0530

    done

--- venv/functions.py
+++ venv/functions.py
@@ -4,13 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 c=[1,2,3,4]
 print('guys')
-array = np.array(c)
-arr= array
+arr=np.array([[1,2,3,4,5],[10,9,8,7,4]],dtype=float,order='C',ndmin=2)
 print(arr)
-print(arr.size)
-a=np.arange(1,10,2,float)
-print(a)
-x=[1,2,3,4,5]
-y=[0.0,0.2,0.4,0.5,0.9]
-plt.plot(x,y,color="red")
-plt.show()
+a = np.arange(24)
+a.ndim
+# now reshape it
+#b = a.reshape(2,4,3)
+#print(b)
+arr=np.zeros((2,2))
+print(arr)
