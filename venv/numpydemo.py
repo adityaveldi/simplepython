ba040a76bf8ba8a566c053c964a1fd3d7ae2bb2f
Veldi <adityaveldi@gmail.com>
2020-05-31 01:05:20 +0530

    done

---
+++ venv/numpydemo.py
@@ -0,0 +1,16 @@
+# Now let us see how to use functions in python
+#dont
+import numpy as np
+import matplotlib.pyplot as plt
+c=[1,2,3,4]
+print('guys')
+arr=np.array([[1,2,3,4,5],[10,9,8,7,4]],dtype=float,order='C',ndmin=2)
+print(arr)
+a = np.arange(24)
+print(arr.ndim)
+print(np.shape(arr))
+# now reshape it
+#b = a.reshape(2,4,3)
+#print(b)
+arr=np.zeros((2,2))
+print(arr)
