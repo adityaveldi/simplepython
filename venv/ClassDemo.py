3eaa7dd24f5fb5f3ccf6ba5ad22b8d4b9aebb64f
Veldi <adityaveldi@gmail.com>
2020-05-31 20:30:39 +0530

    done

---
+++ venv/ClassDemo.py
@@ -0,0 +1,16 @@
+# demo for using classes and using constructors
+class myclass:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def display(self):
+        print('Hey welcome',self.name)
+
+p1=myclass('aditya',20)
+print(p1.name)
+print(p1.age)
+# printing using a method in myclass
+p1.display()
+# if you want to delete the property we can make use of del
+del p1.age
+print('deleted age property of the myclass')
