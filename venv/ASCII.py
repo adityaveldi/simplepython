8c253ab484063ed41701c4ace76f175811ad6e2f
Veldi <adityaveldi@gmail.com>
2020-06-01 21:25:24 +0530

    done

---
+++ venv/ASCII.py
@@ -0,0 +1,3 @@
+# we shall see the ascii value of the given value, we use the ord()
+value=input('Enter the character that you want to know ASCII value:')
+print('The ASCII value of the ',value,' is :',ord(value))
