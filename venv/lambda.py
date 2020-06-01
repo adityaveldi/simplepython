db80a643aae0223bec82c437493ff0a233af8240
Veldi <adityaveldi@gmail.com>
2020-06-02 00:01:38 +0530

    done

---
+++ venv/lambda.py
@@ -0,0 +1,5 @@
+# using lambda function is the anonymous function which accepts the any no of parameters and returns only one value
+x= lambda a,b:a*b
+a=int(input('enter value 1:'))
+b=int(input('enter value 2:'))
+print(x(a,b))
