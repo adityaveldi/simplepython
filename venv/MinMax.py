968efad367be83cd49b9be7415d0153feae39399
Veldi <adityaveldi@gmail.com>
2020-06-02 19:31:14 +0530

    done

---
+++ venv/MinMax.py
@@ -0,0 +1,9 @@
+val1=input('Enter the value1:')
+val2=input('Enter the value2:')
+min=val1
+#print(min)
+if(min<val2):
+    min=val2
+    print('Minimum value is',min)
+else:
+    print('Minimum value is',min)
